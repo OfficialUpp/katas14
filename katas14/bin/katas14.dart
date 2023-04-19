f9ced1c919c771f6356222a3abe244412497cdf7
OfficialUpp <67281407+OfficialUpp@users.noreply.github.com>
2023-04-19 10:44:28 +0100

    Add files via upload
    start project

---
+++ katas14/bin/katas14.dart
@@ -0,0 +1,5 @@
+import 'package:katas14/katas14.dart' as katas14;
+
+void main(List<String> arguments) {
+  print('Hello world: ${katas14.calculate()}!');
+}
---
+++ katas14/test/katas14_test.dart
@@ -0,0 +1,8 @@
+import 'package:katas14/katas14.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
